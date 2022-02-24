ba74d39670d945824d07176659fa30051173791d
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-02-24 10:05:04 +0000

    Initial Commit

---
+++ BranchAndMerge/BranchAndMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranchAndMergeApp.swift
+//  BranchAndMerge
+//
+//  Created by Srinivas Rowdur on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct BranchAndMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
